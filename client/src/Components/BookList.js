7db72fe43a4243469773a2c0424f8d431425a4fd
Bialoskorski <artur.bialoskorski@capgemini.com>
2019-08-05 13:38:17 +0200

    dummy swapi data

--- client/src/Components/BookList.js
+++ client/src/Components/BookList.js
@@ -2,26 +2,50 @@ import React, { Component } from "react";
 import { gql } from "apollo-boost";
 import { graphql } from "react-apollo";
 
-const getBooksQuery = gql`
+const getFilmsQuery = gql`
   {
-    books {
-      name
-      id
+    allFilms {
+      title
     }
   }
 `;
 
-class BookList extends Component {
-  render() {
-    console.log(this.props);
+
+
+class FilmList extends Component {
+    state= {
+        Films: []
+    };
+  // displayFilms() {
+  // var data = this.props.data;
+  //   return data.allFilms.map(film => {
+  //     return <li key={film.id}>{film.title}</li>;
+  //   });
+  // }
+    componentDidMount() {
+        this.setState({
+            Films: this.props.data.allFilms
+        })
+    }
+
+    render() {
+    console.log(this.props.data.allFilms);
+    console.log("STATE " + this.state)
     return (
       <div>
-        <ul id="book-list">
-          <li>Book name</li>
-        </ul>
+        {/*<ul id="film-list">{this.props.data.allFilms}</ul>*/}
+        {/*  <ul>*/}
+        {/*      {this.props.data.allFilms.map(film => (*/}
+        {/*          <div key={film.id}>*/}
+        {/*              <li>*/}
+        {/*                  {film.id}: {film.title}*/}
+        {/*              </li>*/}
+        {/*          </div>*/}
+        {/*      ))}*/}
+        {/*  </ul>*/}
       </div>
     );
   }
 }
 
-export default graphql(getBooksQuery)(BookList);
+export default graphql(getFilmsQuery)(FilmList);
